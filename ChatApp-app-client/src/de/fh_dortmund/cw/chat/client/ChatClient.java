f992a87f728870831267916d5ba438e25148b151
Stefan Dröge <st.droege@gmail.com>
2016-10-12 09:02:48 +0200

    starter

---
+++ ChatApp-app-client/src/de/fh_dortmund/cw/chat/client/ChatClient.java
@@ -0,0 +1,11 @@
+package de.fh_dortmund.cw.chat.client;
+
+import de.fh_dortmund.inf.cw.chat.client.ui.Window;
+
+public class ChatClient {
+
+	public static void main(String[] args) {
+		Window.main(args);
+	}
+
+}
